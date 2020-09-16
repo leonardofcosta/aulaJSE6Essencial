3e07d7a6dc7c37868ffce78a1894c8f30a91ba69
Leonardo F Costa <leonardofcosta@gmail.com>
2020-09-15 23:28:11 -0300

    Estudo sobre tipos de variáveis

---
+++ tipos/1-object.js
@@ -0,0 +1,29 @@
+let user = {
+    name: "Fulano"
+};
+
+console.log(user);
+
+// Alterando a propriedade de um objeto
+user.name = "Outro nome 1";
+
+console.log(user);
+
+user["name"] = "Outro nome 2";
+
+console.log(user);
+
+const prop = "name";
+user[prop] = "Outro nome 3";
+
+console.log(user);
+
+// Criando uma propriedade
+user.lastName = "de Teste";
+
+console.log(user);
+
+// Deletando uma propriedade
+delete user.name;
+
+console.log(user);
---
+++ tipos/2-object.js
@@ -0,0 +1,39 @@
+const user = {
+    name: "Ciclano",
+    lastName: "de Teste"
+};
+
+// Recupera as chaves do objeto
+console.log("Propriedades do objeto user:", Object.keys(user));
+
+// Recupera os valores das chaves do objeto
+console.log("\nValores das propriedades do objeto user:", Object.values(user));
+
+// Retorna um array de arrays contendo [nome_prop, valor_prop]
+console.log("\nLista de propriedades e valores:", Object.entries(user));
+
+// Mergear propriedades de objetos
+Object.assign(user, {fullName: "Ciclano de Teste"});
+
+console.log("\nAdiciona a propriedade fullName no objeto user", user);
+console.log("\nRetorna um novo objeto mergiando dois ou mais objetos", Object.assign({}, user,{age:40}));
+
+// Previne todas as alterações em um objeto
+const newObj = {foo: "bar"};
+Object.freeze(newObj);
+
+newObj.foo = "changes";
+delete newObj.foo;
+newObj.bar = "foo";
+
+console.log("\nVariável newObj após as alterações:", newObj)
+
+//Permite apenas a alteração de propriedades existentes em um objeto
+const person = {name: "Ciclano"};
+Object.seal(person);
+
+person.name = "Ciclano de Teste";
+delete person.name;
+person.age = 26;
+
+console.log("\nVariável person após as alterações:", person)
